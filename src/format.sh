0e78c7267fbcada490be45eb6df58195e490d3a0
Brian Luft <brian@electroly.com>
2025-05-25 14:13:36 -0400

    format.sh: Don't require Developer Command Prompt

--- src/format.sh
+++ src/format.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -eo pipefail
 
-# Check for clang-format.
-if ! command -v clang-format &> /dev/null; then
-    echo "clang-format could not be found."
+VSWHERE="/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe"
+if [ ! -f "$VSWHERE" ]; then
+    echo "Could not find vswhere.exe!"
+    exit 1
+fi
+
+VSPATH=$("$VSWHERE" -all -property installationPath)
+CLANG_FORMAT="$VSPATH\\VC\\Tools\\Llvm\\bin\\clang-format.exe"
+if [ ! -f "$CLANG_FORMAT" ]; then
+    echo "Could not find clang-format.exe!"
     echo "Use Visual Studio Installer to install the clang feature."
-    echo 'Open Developer Command Prompt and run "C:\Program Files\Git\git-bash.exe".'
-    echo "Then, run this script again."
     exit 1
 fi
 
@@ -41,7 +46,7 @@ cd src
 format_project() {
     local project_name=$1
     echo "Formatting $project_name..."
-    find $project_name -type f \( -iname \*.h -o -iname \*.cpp \) | xargs clang-format -i
+    find $project_name -type f \( -iname \*.h -o -iname \*.cpp \) | xargs "$CLANG_FORMAT" -i
 }
 
 format_project "winfile"
